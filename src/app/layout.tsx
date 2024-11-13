c039f101d2026308b2706d6c15ddb6ae23fc5712
Jones Cian <cian.code@hotmail.com>
2024-11-14 00:10:18 +0100

    🚀Begin Supa Auth X project.

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "../styles/globals.css";
+
+export const metadata: Metadata = {
+  title: "Supa Auth X",
+  description: "Supabase Authentication with Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
